98eb0699a73d1a98e562769773e07ba5c6fe4846
gabrielcotua <git@github.com:GabrielCotua/test.git>
2025-04-15 14:51:30 -0500

    developing individual before mixing them in the same file.

--- HW12.c
+++ HW12.c
@@ -20,7 +20,7 @@ int main(void) {
 	int matches[MAXMATCHES], counter = 0;
 	int *piMatches = matches, *piMatchesEnd = matches + MAXMATCHES; // int pointers
 
-	while ( printf("Enter a line of text:\n"), fgets(userInput, SIZE, stdin) && userInput[0] != '\n') {
+	while ( printf("Enter a line of text to search in (ctr+c or ENTER to exit): \n"), fgets(userInput, SIZE, stdin) && userInput[0] != '\n') {
 
 		// cleaning \n character
 		if (myStrCh('\n', userInput) == 0) {
@@ -52,7 +52,12 @@ int main(void) {
 		*/
 
 		printf("The character '%c' was found %d time(s).\n", lookUp, counter);
+
+
+		if (counter > 0) {
+
 		printf("The first %d matches is/are at:", counter = ( (10 >= counter) ? counter:10) );
+
 		// since counter isn't used in the code after, I changed the value to 10 as the maximum.
 
 		// goes through the whole array looking for the matching values
@@ -64,6 +69,10 @@ int main(void) {
 		}
 		printf(" in the string\n");
 		printf("\"%s\"\n ", userInput);
+		} else {
+		printf("No matches.\n");
+
+		}
 
 		// using user input length, we are going to get all the locations of occurrences
 		for (piUserInput = userInput, piMatches = matches; piUserInput < piUserInputEnd; piUserInput++)
@@ -78,6 +87,7 @@ int main(void) {
 		}
 		putchar('\n');
 	}
+	printf("Bye byee.\n");
 
     return 0;
 }
@@ -125,3 +135,53 @@ int myStrCh( const char goal, char pStr[] ){
 
     return 0;
 }
+/*
+
+[Sample output]
+
+|#####################|
+|HW #12, Gabriel Cotua|
+|#####################|
+
+Enter a line of text to search in (ctr+c or ENTER to exit):
+holacomoestamos?
+Your Input: holacomoestamos?
+Input a character to look for:
+The character ' ' was found 5 time(s).
+The first 5 matches is/are at: [17] [18] [19] [20] [21] in the string
+"holacomoestamos?     "
+                 ^^^^^
+Enter a line of text to search in (ctr+c or ENTER to exit):
+buenoksaldfjalksdjflkajsldfkjlaksjdlfjalksdfjaj
+Your Input: buenoksaldfjalksdjflkajsldfkjlaksjdlfja
+Input a character to look for: j
+The character 'j' was found 6 time(s).
+The first 6 matches is/are at: [11] [17] [22] [28] [33] [37] in the string
+"buenoksaldfjalksdjflkajsldfkjlaksjdlfja"
+            ^     ^    ^     ^    ^   ^
+Enter a line of text to search in (ctr+c or ENTER to exit):
+0293845829837459872983479587293874597298345
+Your Input: 029384582983745987298347958729387459729
+Input a character to look for: a
+The character 'a' was found 0 time(s).
+No matches.
+
+Enter a line of text to search in (ctr+c or ENTER to exit):
+a;lsdfj;alskdjf;alsdkfj;alskdfj;alksjdf;alskdjf;lasjkdf;lasjdf;lasjdfla;
+Your Input: a;lsdfj;alskdjf;alsdkfj;alskdfj;alksjdf
+Input a character to look for: ;
+The character ';' was found 5 time(s).
+The first 5 matches is/are at: [1] [7] [15] [23] [31] in the string
+"a;lsdfj;alskdjf;alsdkfj;alskdfj;alksjdf"
+  ^     ^       ^       ^       ^
+Enter a line of text to search in (ctr+c or ENTER to exit):
+;
+Your Input: ;
+Input a character to look for: :
+The character ':' was found 0 time(s).
+No matches.
+
+Enter a line of text to search in (ctr+c or ENTER to exit):
+
+Bye byee.
+*/
